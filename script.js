4a498e2f4dec24f9881002e874017077331b2c80
guidomora <guidomorabito161@hotmail.com>
2022-05-06 10:15:32 -0300

    lunes 9/5

--- script.js
+++ script.js
@@ -1,74 +1,104 @@
 
-
-
-
 class Ramos {
-  constructor(nombre, ramo, precio, flores, stock) {
+  constructor(nombre, ramo, precio, flores) {
     this.nombre = nombre
     this.ramo = ramo
     this.precio = precio
     this.flores = flores
-    this.stock = stock
   }
 }
 
-const ramo1 = new Ramos ("ramo1", "variado chico", 2500, "flores variadas", 30);
-const ramo2 = new Ramos ("ramo2", "rosas", 6000, "rosas rojas", 15);
-const ramo3 = new Ramos ("ramo3", "variado mediano", 3500, "flores variadas", 25);
-const ramo4 = new Ramos ("ramo4", "variado grande", 7000, "flores variadas", 20);
-const carrito = [];
+const ramo1 = new Ramos ("ramo1", "variado chico", 2500, "flores variadas");
+const ramo2 = new Ramos ("ramo2", "rosas", 6000, "rosas rojas");
+const ramo3 = new Ramos ("ramo3", "variado mediano", 3500, "flores variadas");
+const ramo4 = new Ramos ("ramo4", "variado grande", 7000, "flores variadas");
+
+let ramos = [ramo1, ramo2, ramo3, ramo4];
+let buscador = document.getElementById("buscador");
+let divProductos = document.getElementById("divProductos");
+let boton = document.getElementById("boton");
+let costo = 500;
 
 const suma1 = (ramo1, costo) => ramo1 + costo;
 const suma2 = (ramo2, costo) => ramo2 + costo;
 const suma3 = (ramo3, costo) => ramo3 + costo;
 const suma4 = (ramo4, costo) => ramo4 + costo;
 
-let ramos = [ramo1, ramo2, ramo3, ramo4];
-let costo = 500;
-let nombresRamos = prompt("Escriba ramo1, ramo2, ramo3, o ramo4 y en la consola podra ver las caracteristicas de cada uno")
+
+buscador.addEventListener("change", () => {
+  let busqueda = buscador.value;
+  console.log(busqueda.toLowerCase());
+  let ramosFilter = ramos.filter( ramo => ramo.nombre.includes(busqueda.toLowerCase()));
+  
+  ramosFilter.forEach (ramos => {
+    divProductos.innerHTML += `
+        <div>
+          <h3> Ramos ${ramos.nombre} </h3>
+          <p> Tipo de ramo: ${ramos.ramo} </p>
+          <p> Precio: ${ramos.precio} </p>
+          <p> Flores que lleva: ${ramos.flores} </p>
+        </div>
+    `
+  })
+});
+
+boton.addEventListener("click", () => {
+  let boton = buscador.value;
+  alert("Usted eligio " + boton);
+  const envio = confirm("Quiere que se lo enviemos? Tiene un costo de $500")
+    if (envio == true) {
+      alert( + costo)
+    } else if (envio == false) {
+      alert(ramos.precio)
+    }
+})
 
 
-console.log(ramos.find(ramos => ramos.nombre == nombresRamos))
-
-while (nombresRamos !== "ramo1" && nombresRamos !== "ramo2" && nombresRamos !== "ramo3" && nombresRamos !== "ramo4") {
-  alert("producto invalido");
-  nombresRamos = prompt("ingrese ramo1, ramo2, ramo3, o ramo4");
-  console.log(ramos.find(ramos => ramos.nombre == nombresRamos))
-}
+// let costo = 500;
+// let nombresRamos = prompt("Escriba ramo1, ramo2, ramo3, o ramo4 y en la consola podra ver las caracteristicas de cada uno")
 
 
-let producto = prompt("elija un producto");
+// console.log(ramos.find(ramos => ramos.nombre == nombresRamos))
 
-while (producto !== "ramo1" && producto !== "ramo2" && producto !== "ramo3" && producto !== "ramo4") {
-  alert("producto invalido");
+// while (nombresRamos !== "ramo1" && nombresRamos !== "ramo2" && nombresRamos !== "ramo3" && nombresRamos !== "ramo4") {
+//   alert("producto invalido");
+//   nombresRamos = prompt("ingrese ramo1, ramo2, ramo3, o ramo4");
+//   console.log(ramos.find(ramos => ramos.nombre == nombresRamos))
+// }
 
-  producto = prompt("ingrese un producto valido");
-}
 
-const envio = confirm("quiere que lo enviemos?")
+// let producto = prompt("elija un producto");
 
-if (producto == "ramo1" && envio == true) {
-  alert(suma1(ramo1.precio, costo));
-} else if (producto == "ramo1" && (envio == false)) {
-  alert(ramo1.precio);
-}
+// while (producto !== "ramo1" && producto !== "ramo2" && producto !== "ramo3" && producto !== "ramo4") {
+//   alert("producto invalido");
 
-if (producto == "ramo2" && envio == true) {
-  alert(suma1(ramo2.precio, costo));
-} else if (producto == "ramo2" && (envio == false)) {
-  alert(ramo2.precio);
-}
-if (producto == "ramo3" && envio == true) {
-  alert(suma1(ramo3.precio, costo));
-} else if (producto == "ramo3" && (envio == false)) {
-  alert(ramo3.precio);
-}
+//   producto = prompt("ingrese un producto valido");
+// }
 
-if (producto == "ramo4" && envio == true) {
-  alert(suma1(ramo4.precio, costo));
-} else if (producto == "ramo4" && (envio == false)) {
-  alert(ramo4.precio);
-}
+// const envio = confirm("quiere que lo enviemos?")
+
+// if (producto == "ramo1" && envio == true) {
+//   alert(suma1(ramo1.precio, costo));
+// } else if (producto == "ramo1" && (envio == false)) {
+//   alert(ramo1.precio);
+// }
+
+// if (producto == "ramo2" && envio == true) {
+//   alert(suma1(ramo2.precio, costo));
+// } else if (producto == "ramo2" && (envio == false)) {
+//   alert(ramo2.precio);
+// }
+// if (producto == "ramo3" && envio == true) {
+//   alert(suma1(ramo3.precio, costo));
+// } else if (producto == "ramo3" && (envio == false)) {
+//   alert(ramo3.precio);
+// }
+
+// if (producto == "ramo4" && envio == true) {
+//   alert(suma1(ramo4.precio, costo));
+// } else if (producto == "ramo4" && (envio == false)) {
+//   alert(ramo4.precio);
+// }
 
 
 
